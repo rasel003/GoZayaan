8d8bd322e8c40a41973d0dabb9150237de3426f0
RaSeL <codingwithrasel@gmail.com>
2023-12-31 17:06:47 +0600

    added language change test cases

--- app/src/test/java/com/rasel/androidbaseapp/fakes/FakeData.kt
+++ app/src/test/java/com/rasel/androidbaseapp/fakes/FakeData.kt
@@ -1,9 +1,14 @@
 package com.rasel.androidbaseapp.fakes
 
+import com.rasel.androidbaseapp.data.models.Localization
+import com.rasel.androidbaseapp.data.models.LocalizationBundle
+import com.rasel.androidbaseapp.data.models.getLocalization
 import com.rasel.androidbaseapp.domain.models.Character
 import com.rasel.androidbaseapp.domain.models.CharacterLocation
 import com.rasel.androidbaseapp.domain.models.SettingType
 import com.rasel.androidbaseapp.domain.models.Settings
+import com.rasel.androidbaseapp.util.AppLanguage
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
@@ -65,5 +70,33 @@ object FakeData {
             Settings(2, SettingType.TEXT, "App version", "1.0")
         )
         emit(settings)
+    }
+
+    fun getLocalization(language: AppLanguage): Localization {
+        // A Dummy Implementation of fetching Localization from Remote
+        val bundle = LocalizationBundle(
+            en = Localization().apply {
+                lblGreeting = "Hello from Remote"
+                lblSelectedLanguage = "Selected Language From Remote : %@"
+                lblEnglish = "English"
+                lblChinese = "Chinese"
+                lblBurmese = "Burmese"
+            },
+            mm = Localization().apply {
+                lblGreeting = "မင်္ဂလာပါ။ from Remote"
+                lblSelectedLanguage = "ရွေးချယ်ထားသောဘာသာစကား From Remote : %@"
+                lblEnglish = "အင်္ဂလိပ်"
+                lblChinese = "တရုတ်"
+                lblBurmese = "ဗမာ"
+            },
+            cn = Localization().apply {
+                lblGreeting = "你好 from Remote"
+                lblSelectedLanguage = "选择的语言 From Remote : %@"
+                lblEnglish = "英语"
+                lblChinese = "缅甸语"
+                lblBurmese = "中文"
+            }
+        )
+        return bundle.getLocalization(language)
     }
 }
