0487c6e21607945f98567c3c122065ed598f8dea
RaSeL <codingwithrasel@gmail.com>
2024-01-23 17:49:03 +0600

    copied files from iosched app

---
+++ app/src/main/java/com/rasel/androidbaseapp/ui/onboarding/OnboardingFragment.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rasel.androidbaseapp.ui.onboarding
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.rasel.androidbaseapp.databinding.FragmentOnboardingBinding
+import com.rasel.androidbaseapp.util.ViewPagerPager
+import dagger.hilt.android.AndroidEntryPoint
+
+private const val AUTO_ADVANCE_DELAY = 6_000L
+private const val INITIAL_ADVANCE_DELAY = 3_000L
+
+/**
+ * Contains the pages of the onboarding experience and responds to [OnboardingViewModel] events.
+ */
+@AndroidEntryPoint
+class OnboardingFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentOnboardingBinding
+
+    private lateinit var pagerPager: ViewPagerPager
+
+    private val handler = Handler()
+
+    // Auto-advance the view pager to give overview of app benefits
+    private val advancePager: Runnable = object : Runnable {
+        override fun run() {
+            pagerPager.advance()
+            handler.postDelayed(this, AUTO_ADVANCE_DELAY)
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentOnboardingBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+            pager.adapter = OnboardingAdapter(childFragmentManager)
+            pagerPager = ViewPagerPager(pager)
+            // If user touches pager then stop auto advance
+            pager.setOnTouchListener { _, _ ->
+                handler.removeCallbacks(advancePager)
+                false
+            }
+        }
+
+      /*  onboardingViewModel.navigateToMainActivity.observe(viewLifecycleOwner, EventObserver {
+            requireActivity().run {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+        })*/
+
+        return binding.root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        handler.postDelayed(advancePager, INITIAL_ADVANCE_DELAY)
+    }
+
+    override fun onDetach() {
+        handler.removeCallbacks(advancePager)
+        super.onDetach()
+    }
+}
+
+class OnboardingAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+
+    // Don't show then countdown fragment if the conference has already started
+    private val fragments = arrayOf(
+        WelcomePreConferenceFragment(),
+        WelcomePreConferenceFragment(),
+    )
+
+    override fun getItem(position: Int) = fragments[position]
+
+    override fun getCount() = fragments.size
+}
---
+++ app/src/main/java/com/rasel/androidbaseapp/util/Extensions.kt
@@ -0,0 +1,498 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rasel.androidbaseapp.util
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.Resources
+import android.content.res.TypedArray
+import android.graphics.Typeface
+import android.net.wifi.WifiConfiguration
+import android.os.Build
+import android.os.Parcel
+import android.text.Layout.Alignment
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.StaticLayout
+import android.text.TextPaint
+import android.text.style.StyleSpan
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnAttachStateChangeListener
+import android.view.ViewGroup
+import androidx.annotation.ColorInt
+import androidx.annotation.DimenRes
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.os.BuildCompat
+import androidx.core.os.ParcelCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.ObservableBoolean
+import androidx.databinding.ViewDataBinding
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.tasks.Task
+import com.rasel.androidbaseapp.BuildConfig
+import com.rasel.androidbaseapp.data.models.Theme
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.suspendCancellableCoroutine
+import org.threeten.bp.ZonedDateTime
+import timber.log.Timber
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+fun ObservableBoolean.hasSameValue(other: ObservableBoolean) = get() == other.get()
+
+fun Int.isEven() = this % 2 == 0
+
+fun View.isRtl() = layoutDirection == View.LAYOUT_DIRECTION_RTL
+
+inline fun <T : ViewDataBinding> T.executeAfter(block: T.() -> Unit) {
+    block()
+    executePendingBindings()
+}
+
+/**
+ * Calculated the widest line in a [StaticLayout].
+ */
+fun StaticLayout.textWidth(): Int {
+    var width = 0f
+    for (i in 0 until lineCount) {
+        width = width.coerceAtLeast(getLineWidth(i))
+    }
+    return width.toInt()
+}
+
+fun newStaticLayout(
+    source: CharSequence,
+    paint: TextPaint,
+    width: Int,
+    alignment: Alignment,
+    spacingmult: Float,
+    spacingadd: Float,
+    includepad: Boolean
+): StaticLayout {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        StaticLayout.Builder.obtain(source, 0, source.length, paint, width).apply {
+            setAlignment(alignment)
+            setLineSpacing(spacingadd, spacingmult)
+            setIncludePad(includepad)
+        }.build()
+    } else {
+        @Suppress("DEPRECATION")
+        StaticLayout(source, paint, width, alignment, spacingmult, spacingadd, includepad)
+    }
+}
+
+/**
+ * Linearly interpolate between two values.
+ */
+fun lerp(a: Float, b: Float, t: Float): Float {
+    return a + (b - a) * t
+}
+
+/**
+ * Alternative to Resources.getDimension() for values that are TYPE_FLOAT.
+ */
+fun Resources.getFloatUsingCompat(@DimenRes resId: Int): Float {
+    return ResourcesCompat.getFloat(this, resId)
+}
+
+/**
+ * Return the Wifi config wrapped in quotes.
+ */
+fun WifiConfiguration.quoteSsidAndPassword(): WifiConfiguration {
+    return WifiConfiguration().apply {
+        SSID = this@quoteSsidAndPassword.SSID.wrapInQuotes()
+        preSharedKey = this@quoteSsidAndPassword.preSharedKey.wrapInQuotes()
+    }
+}
+
+/**
+ * Return the Wifi config without quotes.
+ */
+fun WifiConfiguration.unquoteSsidAndPassword(): WifiConfiguration {
+    return WifiConfiguration().apply {
+        SSID = this@unquoteSsidAndPassword.SSID.unwrapQuotes()
+        preSharedKey = this@unquoteSsidAndPassword.preSharedKey.unwrapQuotes()
+    }
+}
+
+fun String.wrapInQuotes(): String {
+    var formattedConfigString: String = this
+    if (!startsWith("\"")) {
+        formattedConfigString = "\"$formattedConfigString"
+    }
+    if (!endsWith("\"")) {
+        formattedConfigString = "$formattedConfigString\""
+    }
+    return formattedConfigString
+}
+
+fun String.unwrapQuotes(): String {
+    var formattedConfigString: String = this
+    if (formattedConfigString.startsWith("\"")) {
+        if (formattedConfigString.length > 1) {
+            formattedConfigString = formattedConfigString.substring(1)
+        } else {
+            formattedConfigString = ""
+        }
+    }
+    if (formattedConfigString.endsWith("\"")) {
+        if (formattedConfigString.length > 1) {
+            formattedConfigString =
+                formattedConfigString.substring(0, formattedConfigString.length - 1)
+        } else {
+            formattedConfigString = ""
+        }
+    }
+    return formattedConfigString
+}
+
+/** Make the first instance of [boldText] in a CharSequece bold. */
+fun CharSequence.makeBold(boldText: String): CharSequence {
+    val start = indexOf(boldText)
+    val end = start + boldText.length
+    val span = StyleSpan(Typeface.BOLD)
+    return if (this is Spannable) {
+        setSpan(span, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        this
+    } else {
+        SpannableString(this).apply {
+            setSpan(span, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+    }
+}
+
+/**
+ * Having to suppress lint. Bug raised: 128789886
+ */
+@SuppressLint("WrongConstant")
+fun AppCompatActivity.updateForTheme(theme: Theme) = when (theme) {
+    Theme.DARK -> delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
+    Theme.LIGHT -> delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_NO
+    Theme.SYSTEM -> delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+    Theme.BATTERY_SAVER -> delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+}
+
+/**
+ * Combines this [LiveData] with another [LiveData] using the specified [combiner] and returns the
+ * result as a [LiveData].
+ */
+fun <A, B, Result> LiveData<A>.combine(
+    other: LiveData<B>,
+    combiner: (A, B) -> Result
+): LiveData<Result> {
+    val result = MediatorLiveData<Result>()
+    result.addSource(this) { a ->
+        val b = other.value
+        if (b != null) {
+            result.postValue(combiner(a, b))
+        }
+    }
+    result.addSource(other) { b ->
+        val a = this@combine.value
+        if (a != null) {
+            result.postValue(combiner(a, b))
+        }
+    }
+    return result
+}
+
+/**
+ * Combines this [LiveData] with other two [LiveData]s using the specified [combiner] and returns
+ * the result as a [LiveData].
+ */
+fun <A, B, C, Result> LiveData<A>.combine(
+    other1: LiveData<B>,
+    other2: LiveData<C>,
+    combiner: (A, B, C) -> Result
+): LiveData<Result> {
+    val result = MediatorLiveData<Result>()
+    result.addSource(this) { a ->
+        val b = other1.value
+        val c = other2.value
+        if (b != null && c != null) {
+            result.postValue(combiner(a, b, c))
+        }
+    }
+    result.addSource(other1) { b ->
+        val a = this@combine.value
+        val c = other2.value
+        if (a != null && c != null) {
+            result.postValue(combiner(a, b, c))
+        }
+    }
+    result.addSource(other2) { c ->
+        val a = this@combine.value
+        val b = other1.value
+        if (a != null && b != null) {
+            result.postValue(combiner(a, b, c))
+        }
+    }
+    return result
+}
+
+fun View.doOnApplyWindowInsets(f: (View, WindowInsetsCompat, ViewPaddingState) -> Unit) {
+    // Create a snapshot of the view's padding state
+    val paddingState = createStateForView(this)
+    ViewCompat.setOnApplyWindowInsetsListener(this) { v, insets ->
+        f(v, insets, paddingState)
+        insets
+    }
+    requestApplyInsetsWhenAttached()
+}
+
+/**
+ * Call [View.requestApplyInsets] in a safe away. If we're attached it calls it straight-away.
+ * If not it sets an [View.OnAttachStateChangeListener] and waits to be attached before calling
+ * [View.requestApplyInsets].
+ */
+fun View.requestApplyInsetsWhenAttached() {
+    if (isAttachedToWindow) {
+        requestApplyInsets()
+    } else {
+        addOnAttachStateChangeListener(object : OnAttachStateChangeListener {
+            override fun onViewAttachedToWindow(v: View) {
+                v.requestApplyInsets()
+            }
+
+            override fun onViewDetachedFromWindow(v: View) = Unit
+        })
+    }
+}
+
+private fun createStateForView(view: View) = ViewPaddingState(view.paddingLeft,
+    view.paddingTop, view.paddingRight, view.paddingBottom, view.paddingStart, view.paddingEnd)
+
+data class ViewPaddingState(
+    val left: Int,
+    val top: Int,
+    val right: Int,
+    val bottom: Int,
+    val start: Int,
+    val end: Int
+)
+
+@SuppressLint("NewApi") // Lint does not understand isAtLeastQ currently
+fun DrawerLayout.shouldCloseDrawerFromBackPress(): Boolean {
+    if (BuildCompat.isAtLeastQ()) {
+        // If we're running on Q, and this call to closeDrawers is from a key event
+        // (for back handling), we should only honor it IF the device is not currently
+        // in gesture mode. We approximate that by checking the system gesture insets
+        return rootWindowInsets?.let {
+            val systemGestureInsets = it.systemGestureInsets
+            return systemGestureInsets.left == 0 && systemGestureInsets.right == 0
+        } ?: false
+    }
+    // On P and earlier, always close the drawer
+    return true
+}
+
+/** Compatibility removeIf since it was added in API 24 */
+fun <T> MutableCollection<T>.compatRemoveIf(predicate: (T) -> Boolean): Boolean {
+    val it = iterator()
+    var removed = false
+    while (it.hasNext()) {
+        if (predicate(it.next())) {
+            it.remove()
+            removed = true
+        }
+    }
+    return removed
+}
+
+/** Reads the color attribute from the theme for given [colorAttributeId] */
+fun Context.getColorFromTheme(colorAttributeId: Int): Int {
+    val typedValue = TypedValue()
+    val typedArray: TypedArray =
+        this.obtainStyledAttributes(
+            typedValue.data, intArrayOf(colorAttributeId)
+        )
+    @ColorInt val color = typedArray.getColor(0, 0)
+    typedArray.recycle()
+    return color
+}
+
+
+
+/** Convenience for callbacks/listeners whose return value indicates an event was consumed. */
+inline fun consume(f: () -> Unit): Boolean {
+    f()
+    return true
+}
+
+/**
+ * Allows calls like
+ *
+ * `viewGroup.inflate(R.layout.foo)`
+ */
+fun ViewGroup.inflate(@LayoutRes layout: Int, attachToRoot: Boolean = false): View {
+    return LayoutInflater.from(context).inflate(layout, this, attachToRoot)
+}
+
+/**
+ * Allows calls like
+ *
+ * `supportFragmentManager.inTransaction { add(...) }`
+ */
+inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+    beginTransaction().func().commit()
+}
+
+// region ViewModels
+
+/**
+ * For Actvities, allows declarations like
+ * ```
+ * val myViewModel = viewModelProvider(myViewModelFactory)
+ * ```
+ */
+inline fun <reified VM : ViewModel> FragmentActivity.viewModelProvider(
+    provider: ViewModelProvider.Factory
+) =
+    ViewModelProvider(this, provider).get(VM::class.java)
+
+/**
+ * For Fragments, allows declarations like
+ * ```
+ * val myViewModel = viewModelProvider(myViewModelFactory)
+ * ```
+ */
+inline fun <reified VM : ViewModel> Fragment.viewModelProvider(
+    provider: ViewModelProvider.Factory
+) =
+    ViewModelProvider(this, provider).get(VM::class.java)
+
+/**
+ * Like [Fragment.viewModelProvider] for Fragments that want a [ViewModel] scoped to the Activity.
+ */
+inline fun <reified VM : ViewModel> Fragment.activityViewModelProvider(
+    provider: ViewModelProvider.Factory
+) =
+    ViewModelProvider(requireActivity(), provider).get(VM::class.java)
+
+/**
+ * Like [Fragment.viewModelProvider] for Fragments that want a [ViewModel] scoped to the parent
+ * Fragment.
+ */
+inline fun <reified VM : ViewModel> Fragment.parentViewModelProvider(
+    provider: ViewModelProvider.Factory
+) =
+    ViewModelProvider(parentFragment!!, provider).get(VM::class.java)
+
+// endregion
+// region Parcelables, Bundles
+
+/** Write a boolean to a Parcel. */
+fun Parcel.writeBooleanUsingCompat(value: Boolean) = ParcelCompat.writeBoolean(this, value)
+
+/** Read a boolean from a Parcel. */
+fun Parcel.readBooleanUsingCompat() = ParcelCompat.readBoolean(this)
+
+// endregion
+// region LiveData
+
+/** Uses `Transformations.map` on a LiveData */
+/*fun <X, Y> LiveData<X>.map(body: (X) -> Y): LiveData<Y> {
+    return Transformations.map(this, body)
+}*/
+
+fun <T> MutableLiveData<T>.setValueIfNew(newValue: T) {
+    if (this.value != newValue) value = newValue
+}
+
+// endregion
+
+// region ZonedDateTime
+fun ZonedDateTime.toEpochMilli() = this.toInstant().toEpochMilli()
+// endregion
+
+/**
+ * Helper to force a when statement to assert all options are matched in a when statement.
+ *
+ * By default, Kotlin doesn't care if all branches are handled in a when statement. However, if you
+ * use the when statement as an expression (with a value) it will force all cases to be handled.
+ *
+ * This helper is to make a lightweight way to say you meant to match all of them.
+ *
+ * Usage:
+ *
+ * ```
+ * when(sealedObject) {
+ *     is OneType -> //
+ *     is AnotherType -> //
+ * }.checkAllMatched
+ */
+val <T> T.checkAllMatched: T
+    get() = this
+
+// region Coroutines
+
+/**
+ * Cancel the Job if it's active.
+ */
+fun Job?.cancelIfActive() {
+    if (this?.isActive == true) {
+        cancel()
+    }
+}
+
+// endregion
+
+// region UI utils
+
+// endregion
+
+// region Firebase
+suspend fun <T> Task<T>.suspendAndWait(): T =
+    suspendCancellableCoroutine { continuation ->
+        addOnSuccessListener { result ->
+            continuation.resume(result)
+        }
+        addOnFailureListener { exception ->
+            continuation.resumeWithException(exception)
+        }
+        addOnCanceledListener {
+            continuation.resumeWithException(Exception("Firebase Task was cancelled"))
+        }
+    }
+// endregion
+
+/**
+ * Helper to throw exceptions only in Debug builds, logging a warning otherwise.
+ */
+fun exceptionInDebug(t: Throwable) {
+    if (BuildConfig.DEBUG) {
+        throw t
+    } else {
+        Timber.e(t)
+    }
+}
