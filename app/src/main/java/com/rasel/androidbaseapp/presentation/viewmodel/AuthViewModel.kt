a123a81d7a01997d0458487215eef858fe52183d
RaSeL <codingwithrasel@gmail.com>
2024-02-26 04:08:47 +0600

    refactored BaseViewModel code, not complete yet

--- app/src/main/java/com/rasel/androidbaseapp/presentation/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/rasel/androidbaseapp/presentation/viewmodel/AuthViewModel.kt
@@ -4,17 +4,27 @@ import androidx.lifecycle.MutableLiveData
 import com.rasel.androidbaseapp.data.models.signin.Auth
 import com.rasel.androidbaseapp.data.models.signin.LoginResponse
 import com.rasel.androidbaseapp.data.repository.AuthRepository
+import com.rasel.androidbaseapp.presentation.utils.CoroutineContextProvider
+import com.rasel.androidbaseapp.presentation.utils.ExceptionHandler
 import com.rasel.androidbaseapp.util.ApiResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
 import javax.inject.Inject
 
 @HiltViewModel
 class AuthViewModel @Inject constructor(
+    contextProvider: CoroutineContextProvider,
     private val authRepository: AuthRepository,
-): BaseViewModel2() {
+): BaseViewModel(contextProvider) {
 
     private val _loginResponse = MutableLiveData<ApiResponse<LoginResponse>>()
     val loginResponse = _loginResponse
+
+
+    override val coroutineExceptionHandler = CoroutineExceptionHandler { _, exception ->
+        val message = ExceptionHandler.parse(exception)
+//        _character.postValue(CharacterDetailUIModel.Error(exception.message ?: "Error"))
+    }
 
     fun login(auth: Auth, coroutinesErrorHandler: CoroutinesErrorHandler) = baseRequest(
         _loginResponse,
