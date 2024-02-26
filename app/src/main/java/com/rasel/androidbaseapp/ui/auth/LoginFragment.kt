3458466126be23af94fb14947d6f3989e32488a2
RaSeL <codingwithrasel@gmail.com>
2024-02-26 19:11:02 +0600

    added login code

--- app/src/main/java/com/rasel/androidbaseapp/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/rasel/androidbaseapp/ui/auth/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.rasel.androidbaseapp.ui.auth
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -23,7 +24,8 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, BaseViewModel>() {
     private val tokenViewModel: TokenViewModel by activityViewModels()
 
     private lateinit var navController: NavController
-    override fun getViewBinding(): FragmentLoginBinding = FragmentLoginBinding.inflate(layoutInflater)
+    override fun getViewBinding(): FragmentLoginBinding =
+        FragmentLoginBinding.inflate(layoutInflater)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -36,23 +38,48 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, BaseViewModel>() {
 
         viewModel.loginResponse.observe(viewLifecycleOwner) {
             when (it) {
-                is ApiResponse.Failure -> binding.loginTV.text = it.errorMessage
-                ApiResponse.Loading -> binding.loginTV.text = "Loading"
+                is ApiResponse.Failure -> binding.tvError.text = it.errorMessage
+                ApiResponse.Loading -> binding.tvError.text = "Loading"
                 is ApiResponse.Success -> {
                     tokenViewModel.saveToken(it.data.token)
                 }
             }
         }
 
-        binding.loginButton.setOnClickListener {
-            viewModel.login(
-                Auth("john@mail.com", "changeme"),
-                object : CoroutinesErrorHandler {
-                    override fun onError(message: String) {
-                        binding.loginTV.text = "Error! $message"
+        binding.btnNext.setOnClickListener {
+
+            if (!isPasswordValid(binding.etPassword.text)) {
+                binding.tilPassword.error = getString(R.string.shr_error_password)
+            } else {
+                binding.tilPassword.error = null // Clear the error
+//                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+//                Navigate to the next Fragment
+                viewModel.login(
+                    Auth("john@mail.com", "changeme"),
+                    object : CoroutinesErrorHandler {
+                        override fun onError(message: String) {
+                            binding.tvError.text = "Error! $message"
+                        }
                     }
-                }
-            )
+                )
+            }
+
+
         }
+        // Clear the error once more than 8 characters are typed.
+        binding.etPassword.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(binding.etPassword.text)) {
+                binding.tilPassword.error = null //Clear the error
+            }
+            false
+        }
+    }
+
+    /*
+       In reality, this will have more complex logic including, but not limited to, actual
+       authentication of the username and password.
+    */
+    private fun isPasswordValid(text: Editable?): Boolean {
+        return text != null && text.length >= 8
     }
 }
