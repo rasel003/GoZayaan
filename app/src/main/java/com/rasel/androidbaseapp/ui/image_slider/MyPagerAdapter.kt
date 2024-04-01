068115855b734d133c64cf7c772bbd5f1a7f5dae
RaSeL <codingwithrasel@gmail.com>
2024-04-01 23:04:17 +0600

    converted to data binding

--- app/src/main/java/com/rasel/androidbaseapp/ui/image_slider/MyPagerAdapter.kt
+++ app/src/main/java/com/rasel/androidbaseapp/ui/image_slider/MyPagerAdapter.kt
@@ -3,11 +3,10 @@ package com.rasel.androidbaseapp.ui.image_slider
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.viewpager.widget.PagerAdapter
 import com.bumptech.glide.RequestManager
 import com.rasel.androidbaseapp.R
+import com.rasel.androidbaseapp.databinding.ItemCardBinding
 
 class MyPagerAdapter(
     private val glide: RequestManager,
@@ -19,26 +18,23 @@ class MyPagerAdapter(
     override fun isViewFromObject(view: View, `object`: Any): Boolean = view == `object`
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val view = LayoutInflater
-            .from(container.context)
-            .inflate(R.layout.item_card, container, false)
+
+        val binding =
+            ItemCardBinding.inflate(LayoutInflater.from(container.context), container, false)
 
         val item = items[position]
-        val title: TextView = view.findViewById(R.id.title)
-        val caption: TextView = view.findViewById(R.id.caption)
-        val image: ImageView = view.findViewById(R.id.image)
 
         glide
             .load(item.image)
             .placeholder(R.color.colorPrimaryDark)
             .centerCrop()
-            .into(image)
+            .into(binding.image)
 
-        title.text = item.title
-        caption.text = item.caption
+        binding.title.text = item.title
+        binding.caption.text = item.caption
 
-        container.addView(view)
-        return view
+        container.addView(binding.root)
+        return binding.root
     }
 
     override fun destroyItem(container: ViewGroup, position: Int, view: Any) {
