476637d7b71c01c2536c88b6c13e0852489af97b
RaSeL <codingwithrasel@gmail.com>
2024-05-15 00:10:06 +0600

    refactored item decorator code

--- app/src/main/java/com/rasel/androidbaseapp/ui/mycourses/MyCoursesFragment.kt
+++ app/src/main/java/com/rasel/androidbaseapp/ui/mycourses/MyCoursesFragment.kt
@@ -13,17 +13,15 @@
  */
 package com.rasel.androidbaseapp.ui.mycourses
 
-import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.Px
 import androidx.core.view.doOnNextLayout
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
 import com.materialstudies.owl.model.courses
 import com.rasel.androidbaseapp.R
+import com.rasel.androidbaseapp.core.decorator.BottomSpacingItemDecoration
 import com.rasel.androidbaseapp.databinding.FragmentMyCoursesBinding
 import com.rasel.androidbaseapp.util.SpringAddItemAnimator
 import java.util.concurrent.TimeUnit
@@ -57,20 +55,3 @@ class MyCoursesFragment : Fragment() {
     }
 }
 
-/**
- * A [RecyclerView.ItemDecoration] which adds the given `spacing` to the bottom of any view,
- * unless it is the last item.
- */
-class BottomSpacingItemDecoration(
-    @Px private val spacing: Int
-) : RecyclerView.ItemDecoration() {
-    override fun getItemOffsets(
-        outRect: Rect,
-        view: View,
-        parent: RecyclerView,
-        state: RecyclerView.State
-    ) {
-        val lastItem = parent.getChildAdapterPosition(view) == state.itemCount - 1
-        outRect.bottom = if (!lastItem) spacing else 0
-    }
-}
