fd97d221d55d2418a70bebbb1c15ddef5ca6b7bf
RaSeL <codingwithrasel@gmail.com>
2024-09-27 00:33:00 +0600

    working with bottom sheet system inset

--- app/src/main/java/com/rasel/androidbaseapp/util/FullScreenBottomSheetDialog.kt
+++ app/src/main/java/com/rasel/androidbaseapp/util/FullScreenBottomSheetDialog.kt
@@ -1,11 +1,17 @@
 package com.rasel.androidbaseapp.util
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.marginBottom
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updatePadding
 import androidx.core.view.updatePaddingRelative
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
@@ -69,6 +75,42 @@ class FullScreenBottomSheetDialog : BottomSheetDialogFragment() {
 
         return binding.root
     }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        object : BottomSheetDialog(requireContext(), theme) {
+            override fun onAttachedToWindow() {
+                super.onAttachedToWindow()
+
+                window?.let {
+                    WindowCompat.setDecorFitsSystemWindows(it, false)
+                }
+
+                findViewById<View>(com.google.android.material.R.id.container)?.apply {
+                    fitsSystemWindows = false
+                    ViewCompat.setOnApplyWindowInsetsListener(this) { view, insets ->
+                        insets.apply {
+                            view.updatePadding(bottom = 50)
+                        }
+//                        val insets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                        // Apply the insets as a margin to the view. This solution sets
+                        // only the bottom, left, and right dimensions, but you can apply whichever
+                        // insets are appropriate to your layout. You can also update the view padding
+                        // if that's more appropriate.
+                        /*view.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                            leftMargin = insets.left
+                            bottomMargin = insets.bottom +100
+                            rightMargin = insets.right
+                        }*/
+
+                        // Return CONSUMED if you don't want want the window insets to keep passing
+                        // down to descendant views.
+//                        WindowInsetsCompat.CONSUMED
+                    }
+                }
+
+                findViewById<View>(com.google.android.material.R.id.coordinator)?.fitsSystemWindows = false
+            }
+        }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
